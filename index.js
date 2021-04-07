f7a4857b46d927cb42fffcfba522ae6ddf2625b8
Cipta <42090335+winterbloomingrose@users.noreply.github.com>
2021-04-07 13:24:42 -0400

    Task Four Complete
    Is it just me or is this task just a way easier version of Task three...I feel like this is too good to be true and I'm missing something.

--- index.js
+++ index.js
@@ -107,10 +107,15 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(myCallback) {
+  let scoreKeep = {
+    Home: myCallback();
+    Away: myCallback();
+  }
+  return scoreKeep
 }
 
+console.log(getInningScore(inning));
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
